344f70f559ba67972c2a5ec0bb689dc0bb9e604a
FranciscoGJR <franciscoogjr@gmail.com>
2025-05-16 03:14:58 -0300

    feature: estrutura inicial

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+   "log"
+   "net/http"
+   "os"
+
+   "github.com/gorilla/mux"
+   "github.com/joho/godotenv"
+   "github.com/swaggo/http-swagger"
+   "github.com/FranciscoGJR/mestre-da-colheita/docs"
+   "github.com/FranciscoGJR/mestre-da-colheita/internal/db"
+   "github.com/FranciscoGJR/mestre-da-colheita/internal/handlers"
+)
+
+func main() {
+   godotenv.Load()
+   port := os.Getenv("PORT")
+   if port == "" {
+       port = "8080"
+   }
+
+   database := db.InitDB(os.Getenv("DB_PATH"))
+   r := mux.NewRouter()
+
+   r.PathPrefix("/docs/").Handler(httpSwagger.WrapHandler)
+
+   handlers.RegisterRoutes(r, database)
+
+   log.Printf("Servidor rodando na porta %s", port)
+   log.Fatal(http.ListenAndServe(":"+port, r))
+}
